a650097d032ebfd53d349b040883c956bb07e7db
hsiaomb <hsiaomb@usc.edu>
2016-05-05 02:05:06 -0400

    Edited index functino in views.py and added templates folder

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 
 urlpatterns = [
+	# This will display: /polls/
 	url(r'^$', views.index, name = "index"),
+	# This will display: /polls/5
 	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name = "detail"),
+	# This will display /polls/5/results/
 	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name = "results"),
+	# This will display: /polls/5/vote
 	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name = "vote")
 ]
